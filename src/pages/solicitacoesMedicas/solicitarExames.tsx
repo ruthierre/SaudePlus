6073f94e41a91fc59fd21c17599ef939d466fd4a
Evilazio Neto <evilazioneto943@gmail.com>
2024-01-07 17:37:14 -0300

    Criação das páginas: Solicitar exame e Solicitar Consulta

---
+++ src/pages/solicitacoesMedicas/solicitarExames.tsx
@@ -0,0 +1,23 @@
+import styles from './solicitacoesMedicas.module.css';
+import medicoFicha from '/images/apresentacao/medico-ficha.jpg';
+import texto from '../../data/apresentacao.json'
+import Apresentacao from '../../components/Apresentacao/Apresentacao';
+import FormSolicitacaoMedica from '../../components/FormSolicitacao/FormSolicitacaoMedica/FormSolicitacaoMedica';
+
+function SolicitarExames() {
+    return (
+        <main className={styles.mainSolicMedica}>
+            <h1>Solicitar Exame</h1>
+            <article className={styles.container_solicMedica}>
+                <section>
+                    <FormSolicitacaoMedica tipo='do seu exame'/>
+                </section>
+                <section>
+                    <Apresentacao img={medicoFicha} texto={texto.solicitacao_exames} />
+                </section>
+            </article>
+        </main>
+    )
+}
+
+export default SolicitarExames;
